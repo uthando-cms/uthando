3fe96d9e9db0e528c4117c07e90af40e353d7b33
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-05-04 09:45:12 +0100

    added router for php-cli web server

--- public/index.php
+++ public/index.php
@@ -1,4 +1,24 @@
 <?php
+
+// If we're running under `php -S` with PHP 5.4.0+
+// Thanks to Stephan Sokolow
+if (php_sapi_name() == 'cli-server') {
+
+    // Replicate the effects of basic "index.php"-hiding mod_rewrite rules
+    // Tested working under FatFreeFramework 2.0.6 through 2.0.12.
+    $_SERVER['SCRIPT_NAME'] = str_replace(__DIR__, '', __FILE__);
+    $_SERVER['SCRIPT_FILENAME'] = __FILE__;
+
+    // Replicate the FatFree/WordPress/etc. .htaccess "serve existing files" bit
+    $url_parts = parse_url($_SERVER["REQUEST_URI"]);
+
+    $_req = rtrim($_SERVER['DOCUMENT_ROOT'] . $url_parts['path'], '/' . DIRECTORY_SEPARATOR);
+
+    if (__FILE__ !== $_req && __DIR__ !== $_req && file_exists($_req)) {
+        return false; // serve the requested resource as-is.
+    }
+}
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
