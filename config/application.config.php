53dee63e05c863128001a96600bf52b8c73d78a0
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-05-10 09:51:16 +0100

    layout changes

--- config/application.config.php
+++ config/application.config.php
@@ -14,9 +14,9 @@ return [
 	],
 	'module_listener_options' => [
 		'module_paths' => [
+            './vendor',
+            './devmodules',
 			'./module',
-			'./devmodules',
-			'./vendor',
 		],
 		'config_glob_paths' => [
 			'config/autoload/{,*.}{global,local}.php',
