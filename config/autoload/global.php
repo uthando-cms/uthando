76a99304dcda9f9614183d62cffd73880930f2f2
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-07-08 07:08:37 +0100

    misc changes

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -28,9 +28,4 @@ return [
 		'max_execution_time'      => 60,
 		'date.timezone'           => 'Europe/London',
 	],
-	'theme_manager' => [
-        'default_theme' => 'default',
-        'admin_theme'   => 'admin',
-        'theme_path'    => APPLICATION_PATH . '/public/themes/',
-	],
 ];
