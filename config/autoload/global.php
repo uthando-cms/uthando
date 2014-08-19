7393cb441dbebbcd135dd134030d3af44bb4ce8f
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-08-19 13:22:19 +0100

    removed tabs for spaces

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-	'php_settings' => [
-		'display_startup_errors'  => false,
-		'display_errors'          => true,
-		'error_reporting'         => E_ALL,
-		'max_execution_time'      => 60,
-		'date.timezone'           => 'Europe/London',
-	],
+    'php_settings' => [
+        'display_startup_errors'    => false,
+        'display_errors'            => true,
+        'error_reporting'           => E_ALL,
+        'max_execution_time'        => 60,
+        'date.timezone'             => 'Europe/London'
+    ],
 ];
