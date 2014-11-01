a1052d4841937d2769e1e579b36e362f1c94ce28
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-11-01 11:32:18 +0000

    removed styles and add/edit artilce theme view

--- public/themes/admin/config.php
+++ public/themes/admin/config.php
@@ -3,8 +3,5 @@
 return [
     'template_map' => [
         'layout/layout'                 => __DIR__ . '/view/layout/layout.phtml',
-
-        'uthando-article/article/add'   => __DIR__ . '/view/uthando-article/article/add.phtml',
-        'uthando-article/article/edit'  => __DIR__ . '/view/uthando-article/article/edit.phtml',
     ],
 ];
