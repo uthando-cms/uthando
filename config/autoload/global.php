0b26eea27fb567dc2a9f3395a02ac31bdc3b3d3e
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-03-21 08:59:41 +0000

    updated composer and global config to surpress user defined decricated notices

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -5,7 +5,7 @@ return [
     'php_settings' => [
         'display_startup_errors'        => true,
         'display_errors'                => true,
-        'error_reporting'               => E_ALL,
+        'error_reporting'               => E_ALL ^ E_USER_DEPRECATED,
         'max_execution_time'            => 60,
         'date.timezone'                 => 'Europe/London',
         'zlib.output_compression'       => true,
