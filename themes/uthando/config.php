4365f3b8ea2fe5f8689256741425039c79bd23fc
shaunfreeman <shaun@shaunfreeman.co.uk>
2017-10-12 11:22:09 +0100

    theme additions

--- themes/uthando/config.php
+++ themes/uthando/config.php
@@ -7,6 +7,7 @@ return [
         'widget/blog-archive'   => __DIR__ . '/./view/widgets/blog-archive.phtml',
         'widget/blog-category'  => __DIR__ . '/./view/widgets/blog-category.phtml',
         'widget/blog-popular'   => __DIR__ . '/./view/widgets/blog-popular.phtml',
+        'widget/blog-posts'     => __DIR__ . '/./view/widgets/blog-posts.phtml',
         'widget/blog-recent'    => __DIR__ . '/./view/widgets/blog-recent.phtml',
         'widget/blog-tag-cloud' => __DIR__ . '/./view/widgets/blog-tag-cloud.phtml',
         'widget/search'         => __DIR__ . '/./view/widgets/search.phtml',
